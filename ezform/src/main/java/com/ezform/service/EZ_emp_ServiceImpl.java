baacfbceb9e79683415b92cca47c18374fd3fbe3
oyk0425 <oyk0425@hotmail.com>
2021-09-10 19:18:26 +0900

    EZ_empVO
    회원가입시 비밀번호 default=1, 이후 개인적으로 변경?

--- ezform/src/main/java/com/ezform/service/EZ_emp_ServiceImpl.java
+++ ezform/src/main/java/com/ezform/service/EZ_emp_ServiceImpl.java
@@ -1,5 +1,46 @@
 package com.ezform.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.ezform.domain.EZ_empVO;
+import com.ezform.persistence.EZ_empDAO;
+
+@Service
 public class EZ_emp_ServiceImpl implements EZ_emp_Service {
+
+	@Inject
+	private EZ_empDAO edao;
+	
+	@Override
+	public void empJoin(EZ_empVO vo) {
+		
+		edao.insertEmp(vo);
+		
+	}
+
+	@Override
+	public int updateEmp(EZ_empVO uvo) {
+		
+		return 0;
+	}
+
+	@Override
+	public void deleteEmp(EZ_empVO dvo) {
+		
+		edao.deleteEmp(dvo.getId(), dvo.getName());
+		
+	}
+
+	@Override
+	public List<EZ_empVO> getEmpList(String id) {
+		
+		List<EZ_empVO> empList = edao.getEmpList(id);
+		
+		return null;
+	}
 
 }
