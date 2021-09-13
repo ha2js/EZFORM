125d08fd4fc14a8861bd6280cf26fd1c1f7cba23
josungh <joha3975@naver.com>
2021-09-13 09:01:47 +0900

    Update EZ_empDAOImpl.java

--- ezform/src/main/java/com/ezform/persistence/EZ_empDAOImpl.java
+++ ezform/src/main/java/com/ezform/persistence/EZ_empDAOImpl.java
@@ -1,5 +1,44 @@
 package com.ezform.persistence;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.ezform.domain.EZ_empVO;
+
+@Repository
 public class EZ_empDAOImpl implements EZ_empDAO {
+
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static final String namespace = "com.ezform.mapper.emp_Mapper";
+	
+	@Override
+	public void insertEmp(EZ_empVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateEmp(EZ_empVO updateVO) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteEmp(String id, String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List getEmpList(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
