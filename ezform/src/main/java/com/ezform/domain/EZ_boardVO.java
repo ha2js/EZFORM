28405e437a65f338a326c41fcb54c6f743b8e22f
이영빈 <gmlyb@naver.com>
2021-09-13 12:50:09 +0900

    boardVO,noticeVO
    9.13일 boardVO,noticeVO

--- ezform/src/main/java/com/ezform/domain/EZ_boardVO.java
+++ ezform/src/main/java/com/ezform/domain/EZ_boardVO.java
@@ -1,5 +1,88 @@
 package com.ezform.domain;
 
+import java.sql.Date;
+
 public class EZ_boardVO {
 
+	private int bno;
+	private String id;
+	private String btitle;
+	private String dept;
+	private String name;
+	private Date b_regdate;
+	private String b_content;
+	private int view_cnt;
+	private int like;
+	private String bfile;
+	
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getB_regdate() {
+		return b_regdate;
+	}
+	public void setB_regdate(Date b_regdate) {
+		this.b_regdate = b_regdate;
+	}
+	public String getB_content() {
+		return b_content;
+	}
+	public void setB_content(String b_content) {
+		this.b_content = b_content;
+	}
+	public int getView_cnt() {
+		return view_cnt;
+	}
+	public void setView_cnt(int view_cnt) {
+		this.view_cnt = view_cnt;
+	}
+	public int getLike() {
+		return like;
+	}
+	public void setLike(int like) {
+		this.like = like;
+	}
+	public String getBfile() {
+		return bfile;
+	}
+	public void setBfile(String bfile) {
+		this.bfile = bfile;
+	}
+	
+	@Override
+	public String toString() {
+		return "EZ_boardVO [bno=" + bno + ", id=" + id + ", btitle=" + btitle + ", dept=" + dept + ", name=" + name
+				+ ", b_regdate=" + b_regdate + ", b_content=" + b_content + ", view_cnt=" + view_cnt + ", like=" + like
+				+ ", bfile=" + bfile + "]";
+	}
+	
+	
+	
 }
--- ezform/src/main/java/com/ezform/domain/EZ_noticeVO.java
+++ ezform/src/main/java/com/ezform/domain/EZ_noticeVO.java
@@ -1,5 +1,57 @@
 package com.ezform.domain;
 
+import java.sql.Date;
+
 public class EZ_noticeVO {
 
+	private int nno;
+	private String id;
+	private String ntitle;
+	private String ncontent;
+	private Date n_regdate;
+	
+	public int getNno() {
+		return nno;
+	}
+	public void setNno(int nno) {
+		this.nno = nno;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNtitle() {
+		return ntitle;
+	}
+	public void setNtitle(String ntitle) {
+		this.ntitle = ntitle;
+	}
+	public String getNcontent() {
+		return ncontent;
+	}
+	public void setNcontent(String ncontent) {
+		this.ncontent = ncontent;
+	}
+	public Date getN_regdate() {
+		return n_regdate;
+	}
+	public void setN_regdate(Date n_regdate) {
+		this.n_regdate = n_regdate;
+	}
+	
+	@Override
+	public String toString() {
+		return "EZ_noticeVO [nno=" + nno + ", id=" + id + ", ntitle=" + ntitle + ", ncontent=" + ncontent
+				+ ", n_regdate=" + n_regdate + "]";
+	}
+
+	
+	
+	
+	
+	
+	
+	
 }
