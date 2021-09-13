bad01ba2ae885b3ed3cbfbaad53a9273d5091e98
josungh <joha3975@naver.com>
2021-09-13 09:01:50 +0900

    Update EZ_emp_Service.java

--- ezform/src/main/java/com/ezform/service/EZ_emp_Service.java
+++ ezform/src/main/java/com/ezform/service/EZ_emp_Service.java
@@ -1,5 +1,21 @@
 package com.ezform.service;
 
+import java.util.List;
+
+import com.ezform.domain.EZ_empVO;
+
 public interface EZ_emp_Service {
+	
+	// 직원 계정 생성
+	public void empJoin(EZ_empVO vo);
+	
+	// 직원 정보 수정
+	public int updateEmp(EZ_empVO uvo);
+	
+	// 직원 정보 삭제
+	public void deleteEmp(EZ_empVO dvo);
+	
+	// 직원 목록
+	public List<EZ_empVO> getEmpList(String id);
 
 }
