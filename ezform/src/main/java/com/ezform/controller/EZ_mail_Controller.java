6896767fae1e50fb58f3c5ed795769ce3007f449
Jong Seo Ha <hajongse@naver.com>
2021-09-16 12:25:39 +0900

    update mail_controller

--- ezform/src/main/java/com/ezform/controller/EZ_mail_Controller.java
+++ ezform/src/main/java/com/ezform/controller/EZ_mail_Controller.java
@@ -1,5 +1,38 @@
 package com.ezform.controller;
 
-public class EZ_mail_Controller {
+import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ezform.service.EZ_mail_Service;
+
+
+@Controller
+@RequestMapping("/ez_mail/*")
+public class EZ_mail_Controller {
+	
+	@Inject
+	private EZ_mail_Service service;
+	
+	private static final Logger log = LoggerFactory.getLogger(EZ_mail_Controller.class);
+	
+	@RequestMapping(value="/recMail", method = RequestMethod.GET)
+	public void read_RecMailGET() throws Exception {
+		//  받은 메일
+	}
+	
+	@RequestMapping(value="/sendMail", method = RequestMethod.GET)
+	public void read_SendMailGET() throws Exception {
+		//  보낸 메일
+	}
+	
+	@RequestMapping(value="/writeMail", method = RequestMethod.GET)
+	public void mailWriteGET() throws Exception {
+		// 메일 쓰기
+	}
+	
 }
