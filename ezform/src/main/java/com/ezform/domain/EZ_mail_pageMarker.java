70b9f0e9c0d7792f078d86e32ea5668fd0be5f7c
Jong Seo Ha <hajongse@naver.com>
2021-09-25 14:07:19 +0900

    mail_pageMarker 기존 코드로 수정
    게시판 파트원이 board_pageMaker 파일과 헷갈려서 짠 코드를 기존 짜여진mail_pageMarker 코드로 변경

--- ezform/src/main/java/com/ezform/domain/EZ_mail_pageMarker.java
+++ ezform/src/main/java/com/ezform/domain/EZ_mail_pageMarker.java
@@ -1,106 +1,70 @@
 package com.ezform.domain;
 
-public class EZ_board_PageMaker {
+public class EZ_mail_pageMarker {
 
-	// 페이징 처리 동작 수행
-	private EZ_boardCri Cri; // 페이지에 출력되는 게시판 글의 개수 페이징 처리
-	
-	// 페이지 하단부 페이징 처리
+	private EZ_mailCri cri;
+
 	private int totalCount;
 	private int startPage;
 	private int endPage;
 	private boolean prev;
 	private boolean next;
-	
-	// 페이지 블럭
-	private int displaypageNum = 10;
 
-	public void setCri(EZ_boardCri Cri) {
-		this.Cri = Cri;
-	}
+	private int displayPageNum = 10;
 
-	// 총 개수 계산
+
 	public void setTotalCount(int totalCount) {
 		this.totalCount = totalCount;
 
-		//페이징에  필요한 정보처리
-		CalcData();
-	}
-	
-	public void CalcData() {
-		System.out.println("----- 페이징 처리에 필요한 정보 계산-----");
-		endPage = (int)Math.ceil(Cri.getPageStart()/(double)displaypageNum) * displaypageNum;
-		
-		startPage = (endPage - displaypageNum)+1;
-		
-		int tmpEndPage = (int) Math.ceil(totalCount/(double)Cri.getPageSize());
-		
-		if(endPage > tmpEndPage) {
-			endPage = tmpEndPage;
-		}
-		
-		prev = (startPage == 1? false : true );
-		
-		next = endPage * Cri.getPageSize() >= totalCount? false : true;
-		System.out.println("----- 페이징 처리에 필요한 정보 계산-----");
+		pagingCalc();
 	}
 
-	// get,set
+	public void pagingCalc() {
+		System.out.println("pagingCalc() 호출");
+
+		endPage = (int)Math.ceil(cri.getPageStart()/(double)displayPageNum) * displayPageNum;
+
+		startPage = (endPage - displayPageNum) + 1;
+
+		int tmp = (int)Math.ceil(totalCount/(double)cri.getPageSize());
+
+		if (endPage > tmp) {
+			endPage = tmp;
+		}
+
+		// 이전, 다음 T/F 처리
+		if (startPage == 1) prev = false;
+		else prev = true;
+
+		if ((endPage * cri.getPageSize()) >= totalCount) next = false;
+		else next = true;
+	} 
+
+	public EZ_mailCri getCri() {
+		return cri;
+	}
+
+	public void setCri(EZ_mailCri cri) {
+		this.cri = cri;
+	}
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
 	public int getStartPage() {
 		return startPage;
 	}
-
-	public void setStartPage(int startPage) {
-		this.startPage = startPage;
-	}
-
-	public int getEndPage() {
-		return endPage;
-	}
-
-	public void setEndPage(int endPage) {
-		this.endPage = endPage;
-	}
-
-	public boolean isPrev() {
-		return prev;
-	}
-
-	public void setPrev(boolean prev) {
-		this.prev = prev;
-	}
-
-	public boolean isNext() {
-		return next;
-	}
-
 	public void setNext(boolean next) {
 		this.next = next;
 	}
 
-	public int getDisplaypageNum() {
-		return displaypageNum;
+	public int getDisplayPageNum() {
+		return displayPageNum;
 	}
 
-	public void setDisplaypageNum(int displaypageNum) {
-		this.displaypageNum = displaypageNum;
-	} 
-	
-	public EZ_boardCri getCri() {
-		return Cri;
+	public void setDisplayPageNum(int displayPageNum) {
+		this.displayPageNum = displayPageNum;
 	}
-	
-	public int getTotalCout() {
-		return totalCount;
-	}
-
-	//ToString
-	@Override
-	public String toString() {
-		return "EZ_board_PageMaker [Cri=" + Cri + ", totalCount=" + totalCount + ", startPage=" + startPage
-				+ ", endPage=" + endPage + ", prev=" + prev + ", next=" + next + ", displaypageNum=" + displaypageNum
-				+ "]";
-	}
-	
 
 }
