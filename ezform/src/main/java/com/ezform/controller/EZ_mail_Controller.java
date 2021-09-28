9c4e34770bbe4497f19bd57a61f785aa98749f96
Jong Seo Ha <hajongse@naver.com>
2021-09-28 15:56:25 +0900

    메일 컨트롤러 롤백

--- ezform/src/main/java/com/ezform/controller/EZ_mail_Controller.java
+++ ezform/src/main/java/com/ezform/controller/EZ_mail_Controller.java
@@ -1,10 +1,19 @@
 package com.ezform.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.UUID;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,7 +21,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.ezform.domain.EZ_empVO;
 import com.ezform.domain.EZ_mailCri;
 import com.ezform.domain.EZ_mailVO;
 import com.ezform.domain.EZ_mail_pageMarker;
@@ -31,12 +42,13 @@ public class EZ_mail_Controller {
 	
 	// http://localhost:8088/test/ez_mail/recMail
 	@RequestMapping(value="/recMail", method = RequestMethod.GET)
-	public void read_RecMailGET(EZ_mailCri cri, Model model) throws Exception {
+	public void read_RecMailGET(EZ_mailCri cri, Model model, HttpSession session) throws Exception {
 		//  받은 메일
 		logger.info("read_RecMailGET() 호출");
 		
-		// 임시로 -> 세션 값 불러와서 테스트 (EZ_empVO resultVO)
-		String mail_id = "admin@ezform.com";
+		// 세션
+		EZ_empVO evo = (EZ_empVO)session.getAttribute("resultVO");
+		String mail_id = evo.getEm_email();
 		cri.setMail_id(mail_id);
 		
 		// 수신 메일 list
@@ -54,6 +66,8 @@ public class EZ_mail_Controller {
 	
 	@RequestMapping(value="/recRead", method=RequestMethod.GET)
 	public void readDetail_RecMailGET(@RequestParam("mail_num") int mail_num, Model model) throws Exception {
+		
+		// 메일 읽기
 		logger.info("readDetail_RecMailGET() 호출");
 		
 		EZ_mailVO mvo = service.recRead(mail_num);
@@ -75,19 +89,25 @@ public class EZ_mail_Controller {
 	}
 	
 	@RequestMapping(value="/writeMail", method = RequestMethod.GET)
-	public void mailWriteGET() throws Exception {
+	public void mailWriteGET(Model model, HttpSession session) throws Exception {
 		// 메일 쓰기
 		logger.info("mailWriteGET() 호출");
+		
+		// 세션
+		EZ_empVO evo = (EZ_empVO)session.getAttribute("resultVO");
+		
+		model.addAttribute("write_mail_id",evo.getEm_email());
 	}
 	
 	@RequestMapping(value="/writeMail", method = RequestMethod.POST)
-	public void mailWritePOST(EZ_mailVO vo, Model model, HttpServletResponse response) throws Exception {
+	public void mailWritePOST(EZ_mailVO vo, Model model, HttpServletResponse response, HttpSession session, HttpServletRequest request) throws Exception {
 		// 메일 쓰기		
 		logger.info("mailWritePOST() 호출");
 		
-		// 임시로 -> 세션 값 불러와서 테스트 (EZ_empVO resultVO)
+		// 세션
 		// 보내는 사람
-		String mail_email = "ha2js@ezform.com";
+		EZ_empVO evo = (EZ_empVO)session.getAttribute("resultVO");
+		String mail_email = evo.getEm_email();
 		vo.setMail_email(mail_email);
 		
 		// 보내는 이메일 주소가 em 테이블에 있는지 검사
@@ -100,7 +120,29 @@ public class EZ_mail_Controller {
 			out.flush();
 		}
 		else {
+			// 파일 업로드
+			String fileName = null;
+			MultipartFile uploadFile = vo.getUploadFile();
+			
+			
+			if (!uploadFile.isEmpty()) {
+				String originalFileName = uploadFile.getOriginalFilename();
+				String ext = FilenameUtils.getExtension(originalFileName); // 확장자 구하기
+				UUID uuid = UUID.randomUUID(); //uuid
+				fileName = uuid+"."+ext;
+				
+				String path = request.getSession().getServletContext().getRealPath("/"); // 절대 경로
+				path += "upload\\mailUpload\\";
+				
+				String temp_path = path+fileName;
+				logger.info("파일명 : "+fileName);
+				logger.info("path : "+temp_path);
+				uploadFile.transferTo(new File(path+fileName));
+				
+			}			
+			
 			// 메일 보내기
+			vo.setMail_file(fileName);
 			service.mailWrite(vo);
 			
 			// alert
@@ -108,6 +150,79 @@ public class EZ_mail_Controller {
 			out.flush();
 		}
 	}
+	
+	@RequestMapping(value="/filedown", method=RequestMethod.GET)
+	public void mail_fileDown(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// 파일 다운로드
+		
+		logger.info("mail_fileDown() 호출");
+		
+		String fileName = request.getParameter("fileName");
+		String realFileName = "";
+		
+		logger.info("파일명 : "+fileName);
+		
+		try {
+			String browser = request.getHeader("User-Agent");
+			// 파일 인코딩
+			if (browser.contains("MSIE") || browser.contains("Trident") || browser.contains("Chrome")) {
+				fileName = URLEncoder.encode(fileName,"UTF-8").replaceAll("\\+", "%20");
+			}
+			else {
+				fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+			}
+			
+		} catch(UnsupportedEncodingException ex) {
+			logger.info("UnsupportedEncodingException");
+		}
+		
+		String path = request.getSession().getServletContext().getRealPath("/"); // 절대 경로
+		path += "upload\\mailUpload\\";
+		
+		realFileName = path+fileName;
+		logger.info("절대 경로 : "+realFileName);
+		
+		File tmpFile = new File(realFileName);
+		
+		if (!tmpFile.exists())
+			return;
+		
+		// 파일명 지정
+		response.setContentType("application/octer-stream");
+		response.setHeader("Content-Transfer-Encoding", "binary");
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+		
+		try {
+			OutputStream os = response.getOutputStream();
+			FileInputStream fis = new FileInputStream(realFileName);
+			
+			int Ncnt = 0;
+			byte[] bytes = new byte[512];
+			
+			while ((Ncnt = fis.read(bytes)) != -1) {
+				os.write(bytes,0,Ncnt);
+			}
+			
+			fis.close();
+			os.close();
+			
+		} catch (Exception e) {
+			logger.info("FileNotFoundException : "+e);
+		}
+	}
+	
+	/*
+	public void delete_mail(@RequestParam("delete_mail_num") int[] delete_mail_num) throws Exception {
+		
+		// 체크박스 메일 삭제
+		logger.info("delete_mail() 호출");
+		logger.info("삭제할 메일번호 : "+delete_mail_num);
+		for (int mail_num : delete_mail_num) {
+			
+		}
+	}
+	*/
+	
 	
 	@RequestMapping(value="/sendMail", method = RequestMethod.GET)
 	public void read_SendMailGET() throws Exception {
