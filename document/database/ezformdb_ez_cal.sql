dd9e8b8710cef899b28e22f6066d568b72feb36f
goeunc <rocaille_@kakao.com>
2021-10-04 00:32:01 +0900

    Update ezformdb_ez_cal.sql
    올리는 거 까먹었어요....
    컬럼명 틀에 맞게 수정하려고 해봤는데
    캘린더 작동이 안 되어서 손을 못 댔어요

--- document/database/ezformdb_ez_cal.sql
+++ document/database/ezformdb_ez_cal.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ezformdb
 -- ------------------------------------------------------
@@ -23,26 +23,22 @@ DROP TABLE IF EXISTS `ez_cal`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ez_cal` (
-  `cal_cnum` int(11) NOT NULL,
-  `cal_id` int(11) NOT NULL,
-  `cal_regdate` varchar(100) DEFAULT NULL,
-  `cal_color` varchar(100) DEFAULT NULL,
-  `cal_sche` varchar(1000) DEFAULT NULL,
-  `cal_ck` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`cal_cnum`),
-  KEY `FK_ez_em_TO_ez_cal_1` (`cal_id`),
-  CONSTRAINT `FK_ez_em_TO_ez_cal_1` FOREIGN KEY (`cal_id`) REFERENCES `ez_em` (`em_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `start` timestamp NULL DEFAULT NULL,
+  `end` timestamp NULL DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `userid` int(11) NOT NULL,
+  `backgroundColor` varchar(100) DEFAULT 'rgba(73, 80, 87, 0.2)',
+  `textColor` varchar(100) DEFAULT 'rgba(73, 80, 87, 1)',
+  `borderColor` varchar(100) DEFAULT 'rgba(73, 80, 87, 1)',
+  `allDay` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`_id`),
+  KEY `FK_ez_em_TO_ez_cal_1_idx` (`userid`),
+  CONSTRAINT `FK_ez_em_TO_ez_cal_1` FOREIGN KEY (`userid`) REFERENCES `ez_em` (`em_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ez_cal`
---
-
-LOCK TABLES `ez_cal` WRITE;
-/*!40000 ALTER TABLE `ez_cal` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ez_cal` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-03 14:00:04
+-- Dump completed on 2021-10-04  0:29:13
