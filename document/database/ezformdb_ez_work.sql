c16564d33fe2acd48950fdb9ab0f9b4909ad5729
Jong Seo Ha <hajongse@naver.com>
2021-10-05 15:37:34 +0900

    ez_work 테이블 수정
    primary key : em_id -> work_regdate
    
    work_regdate - default : now()
    
    work_clock -> work_status

--- document/database/ezformdb_ez_work.sql
+++ document/database/ezformdb_ez_work.sql
@@ -23,10 +23,11 @@ DROP TABLE IF EXISTS `ez_work`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ez_work` (
-  `em_id` int(11) NOT NULL,
-  `work_regdate` timestamp NULL DEFAULT NULL,
-  `work_clock` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`em_id`),
+  `em_id` int(11) DEFAULT NULL,
+  `work_regdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `work_status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`work_regdate`),
+  KEY `FK_ez_em_TO_ez_work_1` (`em_id`),
   CONSTRAINT `FK_ez_em_TO_ez_work_1` FOREIGN KEY (`em_id`) REFERENCES `ez_em` (`em_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -49,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-03 14:00:04
+-- Dump completed on 2021-10-05 15:35:28
